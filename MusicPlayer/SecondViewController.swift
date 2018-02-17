ab0a21e3b601d424a0892b6e288f2feb0b1a9f26
Prethush <prethushpj@gmail.com>
2018-02-17 19:56:34 +0530

    Initial Commit

---
+++ MusicPlayer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MusicPlayer
+//
+//  Created by Prethush on 17/02/18.
+//  Copyright © 2018 Prethush. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
